1b8122c16521b0066df82de3eeef556e9cbdb1d2
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-08-16 19:27:17 +0530

    Add files via upload

---
+++ SCRIPT-3.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM sql_store.customers
+
+-- SELECT state 
+-- FROM customers
+
+-- in line 3 and 4 duplicate states will also get selected, to remove the duplicate states from getting selected, we use the DISTINCT keyword
+
+SELECT DISTINCT state
+FROM customers
