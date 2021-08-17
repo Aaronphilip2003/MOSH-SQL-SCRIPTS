cf8c3ad2e7942870b2c835a50265f1d2c3dce9f8
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-08-17 07:28:36 +0530

    Update SCRIPT-5.sql

--- SCRIPT-5.sql
+++ SCRIPT-5.sql
@@ -7,4 +7,6 @@
 --  GET ALL THE CUSTOMERS BORN AFTER 1990 OR POINTS GREATER THAN 1000
 SELECT * 
 FROM sql_store.customers
-WHERE birth_date >= '1990-01-01' OR points > 1000
+-- WHERE birth_date >= '1990-01-01' OR points > 1000
+-- If we want all the data except the one in the above statement we'll use NOT
+WHERE NOT ( birth_date >= '1990-01-01' OR points > 1000)
