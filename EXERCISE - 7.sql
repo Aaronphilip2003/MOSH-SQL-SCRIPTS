65da4001023498fa2bd754245f4c1fdf376ffe40
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-08-18 01:17:36 +0530

    Add files via upload

---
+++ EXERCISE - 7.sql
@@ -0,0 +1,12 @@
+-- Get the customers whose
+-- first names are ELKA or AMBUR
+-- last names end with EY or ON
+-- last names start with MY or contains SE
+-- last names contain B followed by R or U
+
+SELECT *
+FROM sql_store.customers
+-- WHERE first_name REGEXP 'ELKA|AMBUR'
+-- WHERE last_name REGEXP 'EY$|ON$' 
+-- WHERE last_name REGEXP '^MY|SE'
+-- WHERE last_name REGEXP 'B[RU]'
